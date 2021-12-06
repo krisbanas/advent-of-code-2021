30ae6576968b3cde9ca924f608594c64ec4792f1
Banasik Krzysztof <krzysztof.banasik@ti8m.ch>
2021-12-06 06:37:48 +0100

    Day 7

--- src/main/java/com/github/krisbanas/Main.java
+++ src/main/java/com/github/krisbanas/Main.java
@@ -4,6 +4,6 @@ import com.github.krisbanas.solutions.day1.Day6;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Day6().part1());
+        System.out.println(new Day6().part2());
     }
 }
--- src/main/java/com/github/krisbanas/solutions/day1/Day6.java
+++ src/main/java/com/github/krisbanas/solutions/day1/Day6.java
@@ -2,23 +2,88 @@ package com.github.krisbanas.solutions.day1;
 
 import com.github.krisbanas.util.FileHelper;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day6 {
 
-//    private final List<String> inputList;
+    private final List<Fish> fishList;
 
     public Day6() {
-        List<String> inputList = FileHelper.loadFileAsStringList("Day6Input.txt");
+        var tmp = Arrays.stream(FileHelper.loadFileAsString("Day6Input.txt").trim().split(","))
+                .map(Fish::new)
+                .toList();
+        fishList = new ArrayList<>(tmp);
     }
 
     public int part1() {
+        for (int i = 0; i < 256; i++) {
+            System.out.println("Days passed: " + i);
+            passDay();
+        }
 
-        return 0;
+        return fishList.size();
     }
 
-    public int part2() {
+    private void passDay() {
+        List<Fish> toAdd = new ArrayList<>();
+        for (int i = 0; i < fishList.size(); i++) {
+            final var fish = fishList.get(i);
+            var num = fish.getNum();
+            if (num == 0) toAdd.add(new Fish("8"));
+            fish.setNum(num == 0 ? 6 : num - 1);
+        }
+        fishList.addAll(toAdd);
+    }
 
-        return 0;
+    public long part2() {
+        Map<Integer, Long> numOfFishWithDayToBreed = new HashMap<>();
+        for (int i = 0; i <= 8; i++) {
+            numOfFishWithDayToBreed.put(i,0L);
+        }
+
+        fishList.forEach(x -> {
+            var xd = numOfFishWithDayToBreed.get(x.getNum());
+            numOfFishWithDayToBreed.put(x.getNum(), xd + 1L);
+        });
+
+        for (int i = 0; i < 256; i++) {
+            incrementDay(numOfFishWithDayToBreed);
+        }
+
+        return numOfFishWithDayToBreed.values().stream().reduce(0L, Long::sum);
+    }
+
+    private void incrementDay(Map<Integer, Long> numOfFishWithDayToBreed) {
+        long cache =  numOfFishWithDayToBreed.get(8);
+        long current = 0;
+        for (int i = 8; i >= 1; i--) {
+            if (i == 1) {
+                numOfFishWithDayToBreed.put(6, numOfFishWithDayToBreed.get(0) + numOfFishWithDayToBreed.get(6));
+            }
+            current = cache;
+            cache =  numOfFishWithDayToBreed.get(i-1);
+            numOfFishWithDayToBreed.put(i-1, current);
+        }
+        numOfFishWithDayToBreed.put(8, cache);
+    }
+}
+
+class Fish {
+    private int num;
+
+    public Fish(String num) {
+        this.num = Integer.parseInt(num);
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
     }
 }
